c10ff97134005e16672a4415f442f534887c550c
Naginipython <naginipython332@gmail.com>
2024-11-15 11:19:08 -0500

    Triangle without shaders

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,33 @@ int main(int argc, char* argv[]) {
 
   println(glGetString(GL_VERSION));
 
+  // docs.gl
+
+  float positions[6] = {
+    -0.5f, -0.5f,
+    +0.0f, +0.5f,
+    +0.5f, -0.5f
+  };
+
+  // Buffer will just contain the data
+  unsigned int buffer;
+  // Creates id for a buffer
+  glGenBuffers(1, &buffer);
+  // Selects the buffer
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  // Gives data to the buffer. (data_type, size, data, usage)
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  // Controls what the attributes of the buffer are.
+  glEnableVertexAttribArray(0);
+  // At index 0, 2 floats, not normalized, 2 floats to get to next vertex, 0 offset
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.0f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    // Draws the buffer. (mode, starting index, # of vertices)
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
